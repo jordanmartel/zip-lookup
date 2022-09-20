efcc6b042efca4603f6bd5033dea35daeed7b72c
Jordan Martel <jfm_martel@hotmail.com>
2022-09-20 16:59:12 -0400

    Add Apollo client, add simple components to test integration with graphql server

--- zip-lookup-ui/src/App.tsx
+++ zip-lookup-ui/src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useQuery, gql } from '@apollo/client';
+import { useState } from 'react';
+
+const GET_POSTCODE = gql`
+  query Postcode($postCode: String!, $countryCode: String!) {
+    postcode(postCode: $postCode, countryCode: $countryCode) {
+      postCode
+      country
+      countryAbbrev
+      places {
+        name
+        state
+      }
+    }
+  }
+`;
+
+interface Place {
+  name: string,
+  longitude: string,
+  state: string,
+  stateAbbrev: string,
+  latitude: string
 }
 
-export default App;
+function Page() {
+
+    const [postalCode, setPostalCode] = useState("");
+    const [countryCode, setCountryCode] = useState("");
+    const [form, setForm] = useState({ postalCode: "", countryCode: ""})
+
+    return (
+      <div>
+        <div>
+          <h1>Select country and enter postal / zip code </h1>
+          <select value={countryCode} onChange={e => setCountryCode(e.target.value)}>
+          <option value="us">United States</option>
+            <option value="ca">Canada</option>
+          </select>
+          <br/>
+          <br/>
+          <input value={postalCode} onChange={e => setPostalCode(e.target.value)}/>
+          <br/>
+          <br/>
+          <button onClick={e => setForm({ postalCode, countryCode })}>Submit</button>
+        </div>
+        <PostCodeLocation postCode={form.postalCode} countryCode={form.countryCode} />
+      </div>
+    )
+}
+
+interface PostCodeLocationProperties{
+  postCode: string
+  countryCode: string
+}
+
+function PostCodeLocation({postCode, countryCode}: PostCodeLocationProperties) { 
+
+
+  const { loading, error, data } = useQuery(GET_POSTCODE, { variables: { postCode, countryCode }, skip: !postCode || !countryCode}, );
+
+  if (!postCode || !countryCode) {
+    return (
+      <div></div>
+    )
+  }
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Invalid postal code</p>;
+
+  return data.postcode.places.map((place: Place) => (
+    <div key={place.name}>
+      <b>City</b>
+      <p>{place.name}</p>
+      <br />
+      <b>State</b>
+      <p>{place.state}</p>
+      <br />
+    </div>
+
+  ))
+
+}
+
+export default function App() {
+  return (
+    <div>
+    <Page />
+    </div>
+  )
+}
